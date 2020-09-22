4ce281604c85f2bafe8455057964f494f878d1bb
TimChern111 <66830325+TimChern111@users.noreply.github.com>
2020-09-22 17:31:18 +0300

    Fin HW2

--- tusk-7.js
+++ tusk-7.js
@@ -1,8 +1,6 @@
 const input = document.getElementById('font-size-control');
-console.log(input);
 
 const span = document.getElementById('text');
-
 
 input.addEventListener('input', () => {
   span.style.fontSize = input.value + 'px'
